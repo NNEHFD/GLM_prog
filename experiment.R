3d3ddb2b002b3b9522df1db6baad18e046e0840d
NNEHFD <132352413+NNEHFD@users.noreply.github.com>
2025-03-27 09:00:01 +0100

    Update experiment.R

--- experiment.R
+++ experiment.R
@@ -6,39 +6,9 @@
 # Output: 
 #
 # -------------------------------------------------------------------------------------------
-# default_learners list defined by the model, grid for tuning if needed
-default_learners = list( 
-  mars = list(
-    model = mars(mode = "regression", prod_degree = 3) %>%
-      set_engine("earth"),
-    grid = NULL
-  ),
-  
-  lm_pois = list(
-    model = linear_reg() %>%
-      set_engine("glm", family = "poisson"),
-    grid = NULL
-  ),
-  
-  lm_neg = list(
-    model = linear_reg() %>%
-      set_engine("glm", family = negative.binomial(3, link = "log")),
-    grid = NULL
-  ),
-  
-  gbt = list(
-    model = boost_tree(
-      mode = "regression",
-      trees = tune("trees"),
-      tree_depth = tune("tree_depth"),
-      learn_rate = 0.1
-    ) %>%
-      set_engine("xgboost"),
-    grid = expand_grid(trees = seq.int(25, 500, by = 25),
-                       tree_depth = c(3)
-    )
-  )
-)
+library(kknn)
+library(parsnip)
+library(ranger)
 
 experiment = function(n_hist, n_trial,
                       p=p,
@@ -48,88 +18,111 @@ experiment = function(n_hist, n_trial,
   
   outcome_dgp(dgp_string)
   data_hist = dgp(n = n_hist,
-                  p = p, 
+                  p = p,
                   shift_W1 = shift_W1,
-                  shift_U = shift_U) %>% 
+                  shift_U = shift_U) %>%
     mutate(Y = Y0) %>%
     dplyr::select(Y, starts_with('W'))
-  
+
   if(n_hist >= 5000) {V = 3} else if(n_hist >= 1000) {V = 5} else {V = 10}
-  
-  lrnr = PostCard::fit_best_learner(data = data_hist %>% 
-                                    dplyr::select(Y, starts_with('W')), 
+
+  lrnr = PostCard::fit_best_learner(data = data_hist %>%
+                                      dplyr::select(Y, starts_with('W')),
                                     formula = Y ~ .,
                                     cv_folds = V,
-                                    learners = default_learners,
+                                    #learners = default_learners,
                                     verbose = 0)
-  
+
   data_trial = dgp(n = n_trial,
                    p = p, 
                    shift_W1 = 0,
                    shift_U = 0) %>%
     dplyr::select(Y, A, starts_with('W'), m0) #m0 is the oracle prognostic score 
-  
-  data_trial %<>% mutate(prog = predict(lrnr, data_trial %>% 
-                                          dplyr::select(Y, starts_with('W'))) %>% 
-                           pull(.pred) %>% 
-                           pmax(1e-10, .))  # add the estimated prognostic score to the data
-  
-  data_trial %<>% mutate(m0 = link(m0), prog = link(prog)) # add the true prognostic score to the data
 
+  data_trial %<>% mutate(prog = predict(lrnr, data_trial %>%
+                                          dplyr::select(Y, starts_with('W'))) %>%
+                           pull(.pred) %>%
+                           pmax(1e-10, .))  # add the estimated prognostic score to the data
+
+  data_trial %<>% mutate(m0 = link(m0)
+                         , prog = link(prog)
+                         ) # add the true prognostic score to the data
+  
+  cv <- TRUE
   
   bind_rows(
     #unadjusted 
     data_trial %>% 
-      dplyr::select(-prog, -m0) %>%
+      dplyr::select(
+        -prog,
+        -m0) %>%
       rctglm(formula = Y ~ A,
-             group_indicator = A,
+             exposure_indicator = A,
+             exposure_prob = 1/2, 
              family = "gaussian",
              estimand_fun = "rate_ratio",
-             verbose = 0) %>% 
+             verbose = 0,
+             cv_variance = FALSE) %>% 
       estimand() %>% 
       mutate(prog="none", estr="unadjusted"),
     #No prognostic score but glm adjusted with W
     data_trial %>% 
-      dplyr::select(-prog, -m0) %>%
+      dplyr::select(
+        -prog,
+        -m0) %>%
       rctglm(formula = Y ~ .,
-             group_indicator = A,
-             family = negative.binomial(3, link = "log"),
+             exposure_indicator = A,
+             exposure_prob = 1/2, 
+             family = negative.binomial(1/3, link = "log"),
              estimand_fun = "rate_ratio",
-             verbose = 0) %>% 
+             verbose = 0,
+             cv_variance = cv, 
+             cv_variance_folds = 10) %>% 
       estimand() %>% 
       mutate(prog="none", estr="glm"),
     # Random/non-informative prognostic score 
     data_trial %>% 
-      dplyr::select(-prog, -m0) %>%
-      mutate(prog = runif(nrow(data_trial), min = min(data_hist$Y), max = max(data_hist$Y))) %>%
+      dplyr::select(
+        -prog,
+        -m0) %>%
+      mutate(prog = runif(nrow(data_trial), min = 0, max = 10)) %>%
       rctglm(formula = Y ~ .,
-             group_indicator = A,
+             exposure_indicator = A,
+             exposure_prob = 1/2, 
              family = negative.binomial(3, link = "log"),
              estimand_fun = "rate_ratio",
-             verbose = 0) %>% 
+             verbose = 0,
+             cv_variance = cv, 
+             cv_variance_folds = 10) %>% 
       estimand() %>% 
       mutate(prog="fit random", estr="glm"),
     #Prognostic score
-    data_trial %>% 
+    data_trial %>%
       dplyr::select(-m0) %>%
       rctglm(formula = Y ~ .,
-             group_indicator = A,
+             exposure_indicator = A,
+             exposure_prob = 1/2, 
              family = negative.binomial(3, link = "log"),
              estimand_fun = "rate_ratio",
-             verbose = 0) %>% 
-      estimand() %>% 
+             verbose = 0,
+             cv_variance = cv,
+             cv_variance_folds = 10) %>%
+      estimand() %>%
       mutate(prog="fit", estr="glm"),
     # Oracle prognostic score
     data_trial %>% 
-      dplyr::select(-prog) %>%
+      dplyr::select(m0, A, Y) %>%
       rctglm(formula = Y ~ .,
-             group_indicator = A,
+             exposure_indicator = A,
+             exposure_prob = 1/2, 
              family = negative.binomial(3, link = "log"),
              estimand_fun = "rate_ratio",
-             verbose = 0) %>% 
+             verbose = 0,
+             cv_variance = cv,
+             cv_variance_folds = 10) %>% 
       estimand() %>% 
       mutate(prog="oracle", estr="glm")
-) %>%
+  ) %>%
     mutate(
       n_hist = n_hist,
       n_trial = n_trial,
